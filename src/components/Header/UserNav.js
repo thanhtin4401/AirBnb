62fa5d53ef246db17f5d7091bcef310d7a7b0e18
trandinhdanh <dinhdanh183@gmail.com>
2022-10-12 20:12:13 +0700

    feat header 90%

--- src/components/Header/UserNav.js
+++ src/components/Header/UserNav.js
@@ -3,25 +3,25 @@ import { TbWorld } from 'react-icons/tb';
 import { RiAccountCircleFill, RiMenuFill } from 'react-icons/ri';
 import './UserNav.modul.scss';
 import { useNavigate } from 'react-router-dom';
-export default function UserNav() {
+export default function UserNav({bg}) {
   const [open, setOpen] = useState(false);
   const navigate = useNavigate()
   return (
-    <div className="flex items-center lg:flex  md:flex sm:flex  mb:hidden">
-      <h1 className="font-medium text-sm rounded-3xl py-2 px-4 hover:bg-gray-200 transition duration-300">
+    <div className="flex items-center lg:flex  md:flex sm:flex  mb:hidden animate__animated animate__fadeInRight">
+      <h1 className={`${bg ? 'text-white hover:bg-gray-700':'text-black hover:bg-gray-200'} font-medium text-sm rounded-3xl py-2 px-4  transition duration-300`}>
         Trở Thành Chủ Nhà
       </h1>
-      <div className="rounded-3xl py-2 px-2 hover:bg-gray-200 transition duration-300 mx-2">
+      <div className={`${bg ? 'text-white hover:bg-gray-700':'text-black hover:bg-gray-200'} rounded-3xl py-2 px-2 transition duration-300 mx-2`}>
         <TbWorld className="text-xl " />
       </div>
       <div  onClick={() => {
             setOpen(!open);
         }} className="flex items-center py-1 px-4 rounded-3xl border border-gray-300">
-        <RiMenuFill className="text-xl" />
-        <RiAccountCircleFill className="text-3xl" />
+        <RiMenuFill className={`${bg ? 'text-white':'text-black '} text-xl`} />
+        <RiAccountCircleFill className={`${bg ? 'text-white':'text-black '} text-xl`} />
       </div>
-      <div className="dropdownMenu relative">
-        <ul className={`dropdown rounded-xl border border-gray-300 transition duration-500 ${open?"" : "hidden"} `}>
+      <div className="dropdownMenu relative ">
+        <ul className={`animate__animated animate__backInUp  bg-white dropdown rounded-xl border border-gray-300 transition duration-500 ${open?"" : "hidden"} `}>
           <DropDown title={'Đăng Ký'} />
           <DropDown title={'Đăng Nhập'} />
           <div className="bg-gray-300 w-full h-[1px] my-[5px]"></div>
