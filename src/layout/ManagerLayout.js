50720535d6eae08d7a6f8302f4bf2c9f17064ac0
hoangvu0397 <thv0897@gmail.com>
2022-10-18 14:29:05 +0700

    sidebar_update

---
+++ src/layout/ManagerLayout.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Sidebar from '../layout/Sidebar/Sidebar';
+import Footer from '../components/Footer/Footer';
+import Header from '../components/Header/Header';
+
+function ManagerLayout() {
+  return (
+    <div
+      style={{
+        padding: '50px 0px 0px 370px',
+      }}
+    >
+      <Header />
+      <Sidebar />
+      <Outlet />
+    </div>
+  );
+}
+
+export default ManagerLayout;
--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -11,13 +11,23 @@ import Login from '../pages/Login/Login';
 import NoteFoundPage from '../pages/NotFoundPage/NoteFoundPage';
 import SearchPage from '../pages/SearchPage/SearchPage';
 import Register from '../pages/RegisterPage/Register';
-
+import ManagerLayout from '../layout/ManagerLayout';
+import ManagerPage from '../pages/ManagerPage/ManagerPage';
+import 'boxicons/css/boxicons.min.css';
 export default function Routers() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Mainlayout />}>
           <Route path="/" element={<HomePage />}></Route>
+        </Route>
+        <Route path="/" element={<ManagerLayout />}>
+          <Route path="/Manager" element={<ManagerPage />} />
+          <Route index element={<ManagerPage />} />
+          <Route path="/started" element={<ManagerPage />} />
+          <Route path="/calendar" element={<ManagerPage />} />
+          <Route path="/user" element={<ManagerPage />} />
+          <Route path="/order" element={<ManagerPage />} />
         </Route>
         <Route
           path="/"
