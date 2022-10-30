ddaf2d619ed89308a46f329c1e8fb9c35988ecbb
hoangvu0397 <thv0897@gmail.com>
2022-10-30 09:56:22 +0700

    updateold

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -4,9 +4,11 @@ import RequestAuth from '../components/App/requestAuth';
 import RequestPage from '../components/App/requestPage';
 import Authlayout from '../layout/Authlayout';
 import Mainlayout from '../layout/Mainlayout';
+import ManagerLayout from '../layout/ManagerLayout';
 import BookRoomPage from '../pages/BookRoomPage.js/BookRoomPage';
 import HomePage from '../pages/HomePage/HomePage';
 import Login from '../pages/Login/Login';
+import ManagerHome from '../pages/Manager/ManagerHome';
 import NoteFoundPage from '../pages/NotFoundPage/NoteFoundPage';
 
 export default function Routers() {
@@ -35,6 +37,9 @@ export default function Routers() {
           }
         >
           <Route path="/Login" element={<Login />}></Route>
+        </Route>
+        <Route path="/Manager" element={<ManagerLayout />}>
+          <Route path="ManagerHome" element={<ManagerHome />}></Route>
         </Route>
         <Route path="/*" element={<NoteFoundPage />} />
       </Routes>
